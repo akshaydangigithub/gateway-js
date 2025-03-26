36d0072a5ce9ee6632e27d90a48517bd4c08be9c
Hariom dangi <hdangi224@gmail.com>
2025-03-26 19:20:46 +0530

    first commit

---
+++ day1/script.js
@@ -0,0 +1,68 @@
+// console.log("Hello")
+
+// var
+// let
+// const
+
+// name = "Akshay Dangi" // globle scope
+
+// var name1 = "Akshay";
+// let name2 = "Ashutosh";
+// const name3 = "Kuldeep"
+
+// name1 = 'Rohit'
+// name2 = "Sudhir"
+// name3 = "Guru"
+
+// console.log(name3);
+
+// alert("This is alert")
+
+// prompt
+
+// user se input lena
+
+// let ans = prompt("What is ur age ??");
+
+// console.log(typeof ans);
+
+// Data types
+
+// 1. primitive
+
+// number => 2 53
+// string
+// boolean
+// bigInt
+// null
+// undefined
+// symbol
+
+// 2. refrence
+
+// objects
+// array
+// functions
+// dates
+
+// let num = 23.4554;
+
+// console.log(typeof num);
+
+// let ans = false;
+
+// console.log(typeof ans);
+
+// let variable = null;
+
+// console.log(typeof variable);
+
+// type conversion
+
+// console.log(12 + "34.98");
+
+// let variable = "23abc";
+
+// let strToNum = Number(variable)
+
+// console.log(typeof NaN);
