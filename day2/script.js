5dd60b58adbcb4814cb31630f72a21cc1ba74df4
Hariom dangi <hdangi224@gmail.com>
2025-03-27 19:27:00 +0530

    Day2

---
+++ day2/script.js
@@ -0,0 +1,112 @@
+// Today's Topics
+
+// 1. Arithmetic => +,-, * , / , %, incremental, decremental
+// 2. Comparison => > < >= <= == === != !==
+// 3. Logical => && ||
+// 4. Conditionals
+
+// console.log("a+b is:", a + b);
+// console.log("a-b is:", a - b);
+// console.log("a*b is:", a * b);
+// console.log("a/b is:", a / b);
+// console.log("a%b is:", a % b);
+
+// console.log(`a+b is : ${a+b}`);
+
+// ++, --
+
+// console.log("a:", a--);
+
+// console.log(a);
+
+// let a = 12;
+// let b = "12";
+
+// console.log(a !== b);
+
+// console.log(12 < 11 || 23 < 12);
+
+// conditionals
+// if
+// else
+// if else
+// switch case
+
+// if (12 < 45) {
+//   console.log("Chala");
+// } else{
+//     console.log("Else chala");
+
+// }
+
+// let age = Number(prompt("Enter your age"));
+
+// if (age >= 0 && age < 6) {
+//   console.log("You are a kid right now");
+// } else if (age > 6 && age < 18) {
+//   console.log("You are about to reach 18");
+// } else if (age > 18) {
+//   console.log("You can vote");
+// } else {
+//   console.warn("Wrong input");
+// }
+
+// let num = Number(prompt("Please enter a num in between 1-7"));
+
+// if (num <= 7) {
+//   if (num === 1) {
+//     console.log("Sunday");
+//   } else if (num === 2) {
+//     console.log("Monday");
+//   } else if (num === 3) {
+//     console.log("Tuesday");
+//   } else if (num === 4) {
+//     console.log("Wednesday");
+//   } else if (num === 5) {
+//     console.log("Thursday");
+//   } else if (num === 6) {
+//     console.log("Friday");
+//   } else {
+//     console.log("Saturday");
+//   }
+// } else {
+//   console.log("Wrong Input");
+// }
+
+
+let day = prompt("Enter Day Name")
+
+switch (day) {
+  case "monday":
+    console.log("1");
+    break;
+
+  case 2:
+    console.log("Monday");
+    break;
+
+  case 3:
+    console.log("Tuesday");
+    break;
+
+  case 4:
+    console.log("Wednesday");
+    break;
+
+  case 5:
+    console.log("Thursday");
+    break;
+
+  case 6:
+    console.log("Friday");
+    break;
+
+  case 7:
+    console.log("Saturday");
+    break;
+
+  default:
+    console.log("Wrong Input");
+
+    break;
+}
