052a27f561495487598d3d070b25c58fa2e52685
Hariom dangi <hdangi224@gmail.com>
2025-04-24 17:33:42 +0530

    design completed

--- major/script.js
+++ major/script.js
@@ -4,3 +4,15 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+let cart = document.querySelector(".cart");
+let sidebar = document.querySelector(".sidebar");
+let close = document.querySelector(".close");
+
+cart.addEventListener("click", function () {
+  sidebar.classList.remove("translate-x-full");
+});
+
+close.addEventListener("click", function () {
+  sidebar.classList.add("translate-x-full");
+});
